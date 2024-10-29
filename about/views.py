20bc51df15c764a0ca54cf8646ea397209253a2a
Maeve Crossan <maevecrossan@gmail.com>
2024-10-29 13:32:18 +0000

    Add about directory/app

--- about/views.py
+++ about/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from about import views as about_views
 
 # Create your views here.
+def about_me(request):
+    return HttpResponse("This would be the about page")
